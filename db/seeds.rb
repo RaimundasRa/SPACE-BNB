095a46fbcc3dd598534c01deef90cc175d2bd38e
Fiorella Salazar <sajifio@gmail.com>
2022-02-21 17:28:28 +0000

    Added seed file method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating colonies"
+
+20.times do
+  Colony.create{
+    name: Faker::Fantasy::Tolkien.location
+    description: Faker::Movies::StarWars.wookiee_sentence
+    location: Faker::Movies::StarWars.planet
+    population: Faker::Fantasy::Tolkien.race
+    hostility:(1..100).to_a.sample
+  }
+  puts colony.name
+end
