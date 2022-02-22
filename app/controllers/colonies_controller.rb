4d3797ae0b6f6850c6da79b95d83ee2abf07cf3c
DeanTResi <dean@resi.co.uk>
2022-02-22 11:41:10 +0000

    basic links and show page

--- app/controllers/colonies_controller.rb
+++ app/controllers/colonies_controller.rb
@@ -5,5 +5,6 @@ class ColoniesController < ApplicationController
 
   def show
     @colony = Colony.find(params[:id])
+    @fake_year = @colony.created_at.year + @colony.created_at.min + 10000 - @colony.created_at.sec
   end
 end
