06a036dd8413304d5e96d826f896ee59b19b8591
Fiorella Salazar <sajifio@gmail.com>
2022-02-25 11:57:20 +0000

    Added search logic to colonies

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'bootstrap', '~> 5.1.3'
+gem 'pg_search', '~> 2.3.0'
--- app/models/colony.rb
+++ app/models/colony.rb
@@ -2,4 +2,9 @@ class Colony < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: [:name, :description, :location],
+                  using: { tsearch: { prefix: true } }
 end
